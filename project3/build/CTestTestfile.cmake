724e3cd19da935ccfb5a57f678fd4cf7346262a4
Harshal <hardesh1deshpande@gmail.com>
2018-08-04 15:28:41 +0530

    First Commit

---
+++ project3/build/CTestTestfile.cmake
@@ -0,0 +1,11 @@
+# CMake generated Testfile for 
+# Source directory: /home/harshal/project3/src
+# Build directory: /home/harshal/project3/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(lwr_robot/lwr_defs)
+subdirs(forward_kinematics)
+subdirs(robot_sim)
+subdirs(robot_mover)
---
+++ project3/build/robot_mover/CMakeFiles/mover.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mover.dir/src/mover.cpp.o"
+  "/home/harshal/project3/devel/lib/robot_mover/mover.pdb"
+  "/home/harshal/project3/devel/lib/robot_mover/mover"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mover.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
